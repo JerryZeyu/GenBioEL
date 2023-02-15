e44df3320f510ad57ed944727715f2c9698eb19d
JerryZeyu <zzy19951013@gmail.com>
2023-02-15 13:09:34 -0700

    upload changes

--- src/trie/create_trie_and_target_kb.py
+++ src/trie/create_trie_and_target_kb.py
@@ -12,13 +12,13 @@ from trie import Trie
 
 tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
 
-with open('../benchmark/bc5cdr/target_kb.json', 'r') as f:
+with open('../benchmarks/bc5cdr/target_kb.json', 'r') as f:
     cui2str = json.load(f)
 
 entities = []
 for cui in cui2str:
     entities += cui2str[cui]
 trie = Trie([16]+list(tokenizer(' ' + entity.lower())['input_ids'][1:]) for entity in tqdm(entities)).trie_dict
-with open('../benchmark/bc5cdr/trie.pkl', 'wb') as w_f:
+with open('../benchmarks/bc5cdr/trie.pkl', 'wb') as w_f:
     pickle.dump(trie, w_f)
 print("finish running!")
