a56114076dea98d84750d73305f8b341ec717abc
JerryZeyu <zzy19951013@gmail.com>
2023-02-16 10:02:28 -0700

    upload changes

--- src/trie/create_trie_and_target_kb.py
+++ src/trie/create_trie_and_target_kb.py
@@ -36,7 +36,7 @@ tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
 all_country_names = pickle_load_large_file('../benchmarks/lgl/all_country_names.pkl')
 
 
-trie = Trie([11]+list(tokenizer(' ' + entity.lower())['input_ids'][1:]) for entity in tqdm(all_country_names)).trie_dict
+trie = Trie([16]+list(tokenizer(' ' + entity.lower())['input_ids'][1:]) for entity in tqdm(all_country_names)).trie_dict
 with open('../benchmarks/lgl/trie.pkl', 'wb') as w_f:
     pickle.dump(trie, w_f)
 print("finish running!")
