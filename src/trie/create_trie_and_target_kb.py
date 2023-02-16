4c038a317da8c3e1799755aabe3f04f5e2ae6f27
JerryZeyu <zzy19951013@gmail.com>
2023-02-16 10:40:56 -0700

    upload changes

--- src/trie/create_trie_and_target_kb.py
+++ src/trie/create_trie_and_target_kb.py
@@ -19,28 +19,31 @@ def pickle_load_large_file(filepath):
     obj = pickle.loads(bytes_in)
     return obj
 
-tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
-
-with open('../benchmarks/bc5cdr/target_kb.json', 'r') as f:
-    cui2str = json.load(f)
-
-entities = []
-for cui in cui2str:
-    entities += cui2str[cui]
-#print(entities)
-print("-------------------")
-print([list(tokenizer(' ' + entity.lower())['input_ids'][1:]) for entity in entities[0:10]])
-print("**************************")
-trie = Trie([16]+list(tokenizer(' ' + entity.lower())['input_ids'][1:]) for entity in tqdm(entities)).trie_dict
-with open('../benchmarks/bc5cdr/trie.pkl', 'wb') as w_f:
-    pickle.dump(trie, w_f)
-# print("finish running!")
 # tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
 #
-# all_country_names = pickle_load_large_file('../benchmarks/lgl/all_country_names.pkl')
+# with open('../benchmarks/bc5cdr/target_kb.json', 'r') as f:
+#     cui2str = json.load(f)
 #
-#
-# trie = Trie([11]+list(tokenizer(' ' + entity.lower())['input_ids'][1:]) for entity in tqdm(all_country_names)).trie_dict
-# with open('../benchmarks/lgl/trie.pkl', 'wb') as w_f:
+# entities = []
+# for cui in cui2str:
+#     entities += cui2str[cui]
+# #print(entities)
+# print("-------------------")
+# print([list(tokenizer(' ' + entity.lower())['input_ids'][1:]) for entity in entities[0:10]])
+# print("**************************")
+# trie = Trie([16]+list(tokenizer(' ' + entity.lower())['input_ids'][1:]) for entity in tqdm(entities)).trie_dict
+# with open('../benchmarks/bc5cdr/trie.pkl', 'wb') as w_f:
 #     pickle.dump(trie, w_f)
 # print("finish running!")
+tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
+
+all_country_names = pickle_load_large_file('../benchmarks/lgl/all_country_names.pkl')
+
+print(all_country_names)
+print("-------------------")
+print([list(tokenizer(' ' + entity.lower())['input_ids'][1:]) for entity in all_country_names[0:10]])
+print("**************************")
+trie = Trie([11]+list(tokenizer(' ' + entity.lower())['input_ids'][1:]) for entity in tqdm(all_country_names)).trie_dict
+with open('../benchmarks/lgl/trie.pkl', 'wb') as w_f:
+    pickle.dump(trie, w_f)
+print("finish running!")
