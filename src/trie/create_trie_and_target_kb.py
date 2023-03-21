3c5c9490e3defd96cefec64aebbdb79b5110f588
JerryZeyu <zzy19951013@gmail.com>
2023-03-21 14:15:31 -0700

    upload changes

--- src/trie/create_trie_and_target_kb.py
+++ src/trie/create_trie_and_target_kb.py
@@ -43,13 +43,17 @@ def pickle_load_large_file(filepath):
 # print(trie.get([16]))
 tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
 
-all_country_names = pickle_load_large_file('../benchmarks/lgl_withPrompt_country/all_country_names.pkl')
+all_country_names = pickle_load_large_file('../benchmarks/lgl/all_country_names.pkl')
 
 print(all_country_names)
 print("-------------------")
 print([list(tokenizer(' ' + entity.lower())['input_ids'][1:]) for entity in all_country_names[0:10]])
 print("**************************")
 trie = Trie([11]+list(tokenizer(' ' + entity.lower())['input_ids'][1:]) for entity in tqdm(all_country_names)).trie_dict
-with open('../benchmarks/lgl_withPrompt_country/trie.pkl', 'wb') as w_f:
+print(trie.get([11]))
+with open('../benchmarks/lgl/trie.pkl', 'wb') as w_f:
     pickle.dump(trie, w_f)
 print("finish running!")
+with open('../benchmarks/lgl/trie.pkl', 'rb') as r_f:
+    pickle.load(r_f)
+print(trie.get([11]))
