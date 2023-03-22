9d2cd5671cfdc490b6653668bc0b085885aa5cea
JerryZeyu <zzy19951013@gmail.com>
2023-03-21 18:21:12 -0700

    upload changes

---
+++ src/train_GWN_country.sh
@@ -0,0 +1,31 @@
+
+
+DEVICE_NUMBER=0
+MODEL_NAME=GWN_withPrompt_Country
+DATASET=benchmarks
+
+CUDA_VISIBLE_DEVICES=$DEVICE_NUMBER python ./train.py \
+                                            $DATASET/gwn_withPrompt_country \
+                                            -model_load_path facebook/bart-large \
+                                            -model_token_path facebook/bart-large \
+                                            -model_save_path ./model_checkpoints/$MODEL_NAME \
+                                            -save_steps 40000 \
+                                            -logging_path ./logs/$MODEL_NAME \
+                                            -logging_steps 100 \
+                                            -init_lr 1e-05 \
+                                            -per_device_train_batch_size 8 \
+                                            -evaluation_strategy no \
+                                            -label_smoothing_factor 0.1 \
+                                            -max_grad_norm 0.1 \
+                                            -max_steps 40000 \
+					                        -warmup_steps 1000 \
+                                            -weight_decay 0.01 \
+					                        -rdrop 0.0 \
+                                            -lr_scheduler_type polynomial \
+                                            -attention_dropout 0.1  \
+                                            -prompt_tokens_enc 0 \
+                                            -prompt_tokens_dec 0 \
+                                            -max_position_embeddings 1024 \
+                                            -seed 0 \
+					                        -finetune \
+					                        -prefix_mention_is
