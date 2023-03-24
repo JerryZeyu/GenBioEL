0d5a623ec9165c8981055a00a429af7c6dfd5e75
JerryZeyu <zzy19951013@gmail.com>
2023-03-24 09:35:24 -0700

    upload changes

--- src/eval_LGL_feature.sh
+++ src/eval_LGL_feature.sh
@@ -1,15 +1,15 @@
 
 
 DEVICE_NUMBER=0
-MODEL_NAME=LGL_withPrompt_Feature_Debug_4
+MODEL_NAME=LGL_withPrompt_Feature_Debug_5
 DATASET=benchmarks
 
 CUDA_VISIBLE_DEVICES=$DEVICE_NUMBER python ./train.py \
-                                            $DATASET/lgl_withPrompt_feature_debug_4 \
+                                            $DATASET/lgl_withPrompt_feature_debug_5 \
                                             -model_token_path facebook/bart-large \
                                             -evaluation \
-					                        -dict_path $DATASET/lgl_withPrompt_feature_debug_4/target_kb.json \
-                                            -trie_path $DATASET/lgl_withPrompt_feature_debug_4/trie.pkl  \
+					                        -dict_path $DATASET/lgl_withPrompt_feature_debug_5/target_kb.json \
+                                            -trie_path $DATASET/lgl_withPrompt_feature_debug_5/trie.pkl  \
                                             -per_device_eval_batch_size 1 \
 					                        -model_load_path ./model_checkpoints/$MODEL_NAME/checkpoint-40000 \
                                             -max_position_embeddings 1024 \
